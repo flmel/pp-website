eb5d41361a9aadf0047cd389105256d848d66c4a
gagdiez <gagdiez.c@gmail.com>
2022-03-01 20:57:19 +0100

    minor changes

--- assets/js/blockchain/pool.js
+++ assets/js/blockchain/pool.js
@@ -81,16 +81,14 @@ export async function get_pool_info(){
   info.reserve = floor(nearApi.utils.format.formatNearAmount(info.reserve))
   info.prize = floor(nearApi.utils.format.formatNearAmount(info.prize)*.95)
   info.next_prize_tmstmp = (info.next_prize_tmstmp/1000000).toFixed(0)
-  return info  
+  return info
 }
 
 export async function get_last_winners(){
   let imax = await contract.number_of_winners()
-  console.log(imax)
   let imin = (imax >= 10)? imax - 10: 0
 
   let info = await contract.get_winners({from:imin, until:imax})
-  console.log(info)
 
   for(let i=0; i<info.length;i++){
     info[i].amount = floor(nearApi.utils.format.formatNearAmount(info[i].amount))
